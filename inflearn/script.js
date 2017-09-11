bb83719f8e0094681546c7a2a2a52cf1e7165dd1
KimDoyeon <kdy0962@naver.com>
2017-09-11 14:34:28 +0900

    9장
    proAngular 9장 예제 완료

---
+++ inflearn/script.js
@@ -0,0 +1,21 @@
+(function(){
+  var app = angular.module('todo',[]);
+
+  app.controller('TodoCtrl',['$scope',function($scope){
+    $scope.todos = [
+      {
+        title:'요가수련',
+        completed:false,
+        createdAt:Date.now()
+      },{
+        title:'앵귤러 학습',
+        completed:false,
+        createdAt:Date.now()
+      },{
+        title:'운동하기',
+        completed:true,
+        createdAt:Date.now()
+      }
+    ]
+  }]);
+})();
